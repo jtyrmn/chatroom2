dba1783e5ef59c7a0f5e911aef9e85460fab7e4b
jtyrmn <jaketyermanbusiness@gmail.com>
2022-04-27 21:14:34 -0700

    list of selectable rooms

--- site/src/components/RoomSelect.js
+++ site/src/components/RoomSelect.js
@@ -1,18 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 //for the user to select a chatroom
 
 function RoomSelect({socket}){
+    const [rooms, setRooms] = useState([]);
 
     //change into room with id
-    const change_room = () => {
-        const id = document.getElementById('room_input').value;
-
-        socket.emit('change_room', id)
+    const change_room = (id) => {
+        socket.emit('change_room', id);
     }
 
+
+    useEffect(() => {
+        axios.get('/rooms')
+        .then(response => {
+            setRooms(response.data);
+        })
+        .catch(err => {
+            document.getElementById('room-list').innerHTML = `unable to retrieve room data\n ${err}`;
+        });
+    }, []);
+
     return (
-        <div>
-            <input id="room_input"></input>
-            <button onClick={change_room}>change room</button>
+        <div id="room-list">
+            Rooms
+            {
+                rooms.map(room => <li key={room.room_id}> {room.room_id}:{room.creator_name}:{room.room_name} <button onClick={() => {change_room(room.room_id)}}>join</button></li>)
+            }
         </div>
     );
 }
